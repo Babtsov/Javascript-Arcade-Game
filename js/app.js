1b6d528969ccdea4931980d935319f21ac467211
Ben Babtsov <ben.babtsov@ufl.edu>
2015-12-24 19:22:29 -0500

    Add enemies to board

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(initX,initY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 101*initX;
+    this.y = -20+82*initY;
+    this.speed = this.getRandomSpeed();
 };
 
 // Update the enemy's position, required method for game
@@ -14,10 +17,19 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed*dt;
+};
+
+Enemy.prototype.getRandomSpeed = function(){
+    return Math.floor((Math.random() * 300) + 50);
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    if (this.x > 500) {
+        this.x = -101;
+        this.speed = this.getRandomSpeed();
+    }
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -25,10 +37,11 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies =[new Enemy(-2,1,200),new Enemy(-1,2,30),new Enemy(-1,3,50), new Enemy(-5,2,100)];
+
 
 
 
@@ -41,6 +54,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+    console.log("hey");
     player.handleInput(allowedKeys[e.keyCode]);
 });
